315ff4d008f695d5d45e6686ac836ff33300075d
Angel Juarez <angeljuarez729@gmail.com>
2019-05-25 00:45:49 -0400

    init commit

---
+++ test/first_lein_proj/core_test.clj
@@ -0,0 +1,7 @@
+(ns first-lein-proj.core-test
+  (:require [clojure.test :refer :all]
+            [first-lein-proj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
